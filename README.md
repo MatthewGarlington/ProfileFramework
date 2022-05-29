d26b3bbfebb144aabfb7579f2c99005f77efee31
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-05-29 09:46:18 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProfileFramework
+
+A description of this package.
---
+++ Sources/ProfileFramework/ProfileFramework.swift
@@ -0,0 +1,6 @@
+public struct ProfileFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
