679b14ddbb1b0bd250032d1325bca943846c722c
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-06-04 14:05:59 -0400

    feat: Added Firebase Package and initailized a class to reference a create store in Firebase store for testing

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.34.0")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.34.0"),
+        .package(name: "Firebase",
+                   url: "https://github.com/firebase/firebase-ios-sdk.git",
+                   from: "8.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +26,8 @@ let package = Package(
         .target(
             name: "ProfileFramework",
             dependencies: [
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "FirebaseFirestore", package: "Firebase")
             ])
     ]
 )
--- Sources/ProfileFramework/Networking/FirestoreManager.swift
+++ Sources/ProfileFramework/Networking/FirestoreManager.swift
@@ -5,10 +5,36 @@
 //  Created by Matthew Garlington on 6/4/22.
 //
 
-//import SwiftUI
-//import Firebase
-// 
-//
-//class FirestoreManager: ObservableObject {
-//    
-//}
+import SwiftUI
+import Firebase
+ 
+
+class FirestoreManager: ObservableObject {
+    @Published var post: String = ""
+    
+    init() {
+        fetchPost()
+    }
+    
+    func fetchPost() {
+        let dataBase = Firestore.firestore()
+        
+        let documentReference = dataBase.collection("Post").document("Matthew Garlington")
+        
+        documentReference.getDocument { document, error in
+            guard error == nil else {
+                print("\(error!.localizedDescription)")
+                return
+            }
+            
+            if let document = document, document.exists {
+                let data = document.data()
+                if let data = data {
+                    print("Data: \(data)")
+                    self.post = data["name"] as? String ?? ""
+                }
+            }
+        }
+        
+    }
+}
